8e21987220b7f666e6fef63cad7824d3c48140ae
Dhruv97Sharma <ddhuvsharma1997@gmail.com>
2023-11-26 00:44:51 +0530

    Added cors middleware for handling api requests from the nextjs client app

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from tweety import generate_sde_tweet, generate_tweet_hashtags, validate_prompt
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
